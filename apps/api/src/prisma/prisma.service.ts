3cb83df08f8c880282e60d58ca38bcb7006d73cd
PSJ84 <psj@hkst.kr>
2025-12-10 21:10:21 +0900

    Change PrismaClient import source to @prisma/client

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,6 +1,6 @@
 // apps/api/src/prisma/prisma.service.ts
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@solar-pm/prisma';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
